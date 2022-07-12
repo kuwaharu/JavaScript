8e3af686f09dc678bbb8353a78b1b5423febe96c
Kuwamura <jobtech.kumamura@gmai.com>
2022-07-12 21:16:22 +0900

    Fixed.

--- JavaScript.js
+++ JavaScript.js
@@ -2,7 +2,7 @@ const Fizz = 3;
 const Buzz = 5;
 
 for (let i = 1; i <= 99; i++) {
-  if ((i%3 === 0) && (i%5 === 0))  {
+  if ((i % 3 === 0) && (i % 5 === 0))  {
     console.log("FizzBuzz\t" + i);
   } else if (i % Buzz === 0) {
     console.log("Buzz\t" + i);
