82761cd641c6565b0fd378b4aa675a41b4540617
Kuwamura <jobtech.kumamura@gmai.com>
2022-08-11 00:10:41 +0900

    Add.

---
+++ 新しいフォルダー/javascript.js
@@ -0,0 +1,11 @@
+// example01
+const example = ['foo', 'bar', 'baz'];
+console.dir(example);
+
+// example02
+const nirvana = [
+  { 'Name': 'Kurt Cobain', 'Part': 'Vocal / Guitar' },
+  { 'Name': 'Krist Novoselic', 'Part': 'Bass' },
+  { 'Name': 'Dave Grohl', 'Part': 'Drums' },
+];
+console.dir(nirvana);
